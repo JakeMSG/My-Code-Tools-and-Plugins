54c0155a32ca1d8cc92f9242d376e9271c276b15
JakeMSG <107895377+JakeMSG@users.noreply.github.com>
2026-07-04 12:56:21 +0300

    Add files via upload

--- Plugins I Modified & Made/RPGMaker MV&MZ/# My Creations/Easier Script Calls/JakeMSG_EasierScriptCallsMVMZ.js
+++ Plugins I Modified & Made/RPGMaker MV&MZ/# My Creations/Easier Script Calls/JakeMSG_EasierScriptCallsMVMZ.js
@@ -12,9 +12,14 @@ Imported.JakeMSG_EasierScriptCallsMVMZ = true;
  * @plugindesc Easier Script Calls for MV&MZ (method wrappers to make Script Calls 
  * easier to use)
  * @author JakeMSG
- * v1.1
+ * v1.2
  * 
 ============ Change Log ============
+1.2 - 7.4th.2026
+ * Added "origin" parameter to picRotate, picAddAngle, and picSetAngle (rotation anchor: top-left or center)
+ * Added picAddAngleBySpeed and picSetAngleBySpeed (animated angle change by speed, with optional ease in/out)
+ * Added picAddAngleByTime and picSetAngleByTime (animated angle change over a set number of frames)
+ * Fixed plugin load failure caused by missing semicolon before the Game_Picture setup IIFE
 1.1 - 2.15th.2026
  * Added the Script call for Plugin Commands (useful when using the MZ editor for an MV project)
 1.0 - 2.13th.2026
@@ -110,24 +115,79 @@ Imported.JakeMSG_EasierScriptCallsMVMZ = true;
  * 
  * 
  * ============ Rotate (starts moving by speed)
- * picRotate(picID=1, speed=0)
+ * picRotate(picID=1, speed=0, origin=0)
  * 
  * picID = ID of the Picture to modify
  * speed = Speed to set the Picture to Rotate (clockwise if Positive, counter-Clockwise if Negative), the rotation
  *         being (speed/2) degrees per frame (0 means no rotation / Stop rotation)
+ * origin = 0 (Rotation anchor is the Upper-Left corner) or 1 (Rotation anchor is the Center of the picture)
+ *         When the origin differs from the picture's current anchor, the picture position is recalculated
+ *         so the image stays in place while the rotation pivot changes.
  * 
  * 
- * ============ Add Angle (adds to the current angle)
- * picAddAngle(index=1, angle=0)
- * 
- * picID = ID of the Picture to modify
- * angle = Adds to the current angle (turns around the X&Y anchor point; clockwise if Positive, counter-Clockwise if Negative)
- * 
- * ============ Set Angle (sets the angle)
- * picSetAngle(index=1, angle=0)
+ * ============ Add Angle (adds to the current angle, instantly)
+ * picAddAngle(picID=1, angle=0, origin=0)
  * 
  * picID = ID of the Picture to modify
- * angle = Sets the current angle (turns around the X&Y anchor point; clockwise if Positive, counter-Clockwise if Negative)
+ * angle = Adds to the current angle (clockwise if Positive, counter-Clockwise if Negative)
+ * origin = 0 (Rotation anchor is the Upper-Left corner) or 1 (Rotation anchor is the Center of the picture)
+ *         When the origin differs from the picture's current anchor, the picture position is recalculated
+ *         so the image stays in place while the rotation pivot changes.
+ * 
+ * ============ Set Angle (sets the angle, instantly)
+ * picSetAngle(picID=1, angle=0, origin=0)
+ * 
+ * picID = ID of the Picture to modify
+ * angle = Sets the current angle (clockwise if Positive, counter-Clockwise if Negative)
+ * origin = 0 (Rotation anchor is the Upper-Left corner) or 1 (Rotation anchor is the Center of the picture)
+ *         When the origin differs from the picture's current anchor, the picture position is recalculated
+ *         so the image stays in place while the rotation pivot changes.
+ * 
+ * 
+ * ============ Add Angle by Speed (adds to the current angle, with visual movement)
+ * picAddAngleBySpeed(picID=1, angle=0, origin=0, speed=1.5, easeInTime=0, easeOutTime=0)
+ * 
+ * picID = ID of the Picture to modify
+ * angle = Adds to the current angle (clockwise if Positive, counter-Clockwise if Negative)
+ * origin = 0 (Rotation anchor is the Upper-Left corner) or 1 (Rotation anchor is the Center of the picture)
+ *         When the origin differs from the picture's current anchor, the picture position is recalculated
+ *         so the image stays in place while the rotation pivot changes.
+ * speed = Degrees per frame the picture rotates until the target angle is reached (accepts float values, e.g. 1.5)
+ * easeInTime = Frames for the rotation speed to linearly ramp from 0 to the top speed (0 = start at top speed instantly)
+ * easeOutTime = Frames for the rotation speed to linearly ramp from the top speed down to 0 near the target angle (0 = no ease-out)
+ * 
+ * ============ Set Angle by Speed (sets the angle, with visual movement)
+ * picSetAngleBySpeed(picID=1, angle=0, origin=0, speed=1.5, easeInTime=0, easeOutTime=0)
+ * 
+ * picID = ID of the Picture to modify
+ * angle = Sets the target angle (clockwise if Positive, counter-Clockwise if Negative)
+ * origin = 0 (Rotation anchor is the Upper-Left corner) or 1 (Rotation anchor is the Center of the picture)
+ *         When the origin differs from the picture's current anchor, the picture position is recalculated
+ *         so the image stays in place while the rotation pivot changes.
+ * speed = Degrees per frame the picture rotates until the target angle is reached (accepts float values, e.g. 1.5)
+ * easeInTime = Frames for the rotation speed to linearly ramp from 0 to the top speed (0 = start at top speed instantly)
+ * easeOutTime = Frames for the rotation speed to linearly ramp from the top speed down to 0 near the target angle (0 = no ease-out)
+ * 
+ * 
+ * ============ Add Angle by Time (adds to the current angle, with visual movement)
+ * picAddAngleByTime(picID=1, angle=0, origin=0, time=60)
+ * 
+ * picID = ID of the Picture to modify
+ * angle = Adds to the current angle (clockwise if Positive, counter-Clockwise if Negative)
+ * origin = 0 (Rotation anchor is the Upper-Left corner) or 1 (Rotation anchor is the Center of the picture)
+ *         When the origin differs from the picture's current anchor, the picture position is recalculated
+ *         so the image stays in place while the rotation pivot changes.
+ * time = Number of frames the rotation movement should take to reach the target angle
+ * 
+ * ============ Set Angle by Time (sets the angle, with visual movement)
+ * picSetAngleByTime(picID=1, angle=0, origin=0, time=60)
+ * 
+ * picID = ID of the Picture to modify
+ * angle = Sets the target angle (clockwise if Positive, counter-Clockwise if Negative)
+ * origin = 0 (Rotation anchor is the Upper-Left corner) or 1 (Rotation anchor is the Center of the picture)
+ *         When the origin differs from the picture's current anchor, the picture position is recalculated
+ *         so the image stays in place while the rotation pivot changes.
+ * time = Number of frames the rotation movement should take to reach the target angle
  * 
  * 
  * 
@@ -227,30 +287,87 @@ Imported.JakeMSG_EasierScriptCallsMVMZ = true;
 
 // ================================================ Functions
 // ================================ Plugin Commands
-var plugCall = function() {
-    if (arguments[0]){
-        var args = arguments[0].split(" ");
-        var command = args.shift();
-    } else {
-        var args = "";
-        var commands = "";
+var plugCall = function(pluginCommands) {
+    if (!pluginCommands || typeof pluginCommands !== 'string') {
+        return;
     }
+    var args = pluginCommands.split(" ");
+    var command = args.shift();
     if ($gameParty.inBattle()) { // In Battle (relevant for choosing which Interpreter to use)
         $gameTroop._interpreter.pluginCommand(command, args);
     } else { // On Map (relevant for choosing which Interpreter to use)
         $gameMap._interpreter.pluginCommand(command, args);
     }
-}
-
-
-
+};
 
 // ================================================ Visual
 // ================================ Pictures
+var _jakeGetPic = function(picID) {
+    return $gameScreen.picture(picID);
+};
+
+var _jakeRotatePoint = function(x, y, angleDeg) {
+    var rad = angleDeg * Math.PI / 180;
+    return {
+        x: x * Math.cos(rad) + y * Math.sin(rad),
+        y: -x * Math.sin(rad) + y * Math.cos(rad)
+    };
+};
+
+var _jakeGetPictureScaledSize = function(pic) {
+    var name = pic._name;
+    if (!name) {
+        return { w: 0, h: 0 };
+    }
+    var bitmap = ImageManager.loadPicture(name);
+    if (!bitmap || bitmap.width <= 0 || bitmap.height <= 0) {
+        return { w: 0, h: 0 };
+    }
+    return {
+        w: bitmap.width * pic._scaleX / 100,
+        h: bitmap.height * pic._scaleY / 100
+    };
+};
+
+var _jakeGetOriginAnchorLocal = function(origin, w, h) {
+    if (origin === 0) {
+        return { x: 0, y: 0 };
+    }
+    return { x: w / 2, y: h / 2 };
+};
+
+var _jakeApplyOriginIfChanged = function(pic, newOrigin) {
+    var oldOrigin = pic._origin;
+    if (oldOrigin === newOrigin) {
+        return;
+    }
+    var size = _jakeGetPictureScaledSize(pic);
+    if (size.w <= 0 || size.h <= 0) {
+        pic._origin = newOrigin;
+        return;
+    }
+    var angle = pic._angle || 0;
+    var oldAnchor = _jakeGetOriginAnchorLocal(oldOrigin, size.w, size.h);
+    var oldRot = _jakeRotatePoint(oldAnchor.x, oldAnchor.y, angle);
+    var topLeftX = pic._x - oldRot.x;
+    var topLeftY = pic._y - oldRot.y;
+    var newAnchor = _jakeGetOriginAnchorLocal(newOrigin, size.w, size.h);
+    var newRot = _jakeRotatePoint(newAnchor.x, newAnchor.y, angle);
+    var newX = topLeftX + newRot.x;
+    var newY = topLeftY + newRot.y;
+    var deltaX = newX - pic._x;
+    var deltaY = newY - pic._y;
+    pic._x = newX;
+    pic._y = newY;
+    pic._targetX += deltaX;
+    pic._targetY += deltaY;
+    pic._origin = newOrigin;
+};
+
 // ================ Show (/Remove (by leaving the picName blank) )
 var picChange = function(picName="", picID=1, origin=0, x=0, y=0, scaleX=100, scaleY=100, opacity=255, blendMode=0) {
     $gameScreen.showPicture(picID, picName, origin, x, y, scaleX, scaleY, opacity, blendMode);
-}
+};
 
 // ================ Move
 var picMove = function(picID=1, origin=0, x=0, y=0, scaleX=100, scaleY=100, opacity=255, blendMode=0, frames=0, easing=0) {
@@ -259,22 +376,173 @@ var picMove = function(picID=1, origin=0, x=0, y=0, scaleX=100, scaleY=100, opac
     } else { // 'MV'
         $gameScreen.movePicture(picID, origin, x, y, scaleX, scaleY, opacity, blendMode, frames);
     }
-}
+};
 
 // ================ Rotate (starts moving by speed)
-var picRotate = function(picID=1, speed=0) {
+var picRotate = function(picID=1, speed=0, origin=0) {
+    var pic = _jakeGetPic(picID);
+    if (pic) {
+        pic._jakeCancelAngleAnim();
+        _jakeApplyOriginIfChanged(pic, origin);
+    }
     $gameScreen.rotatePicture(picID, speed);
-}
+};
 
 // ================ Add Angle (adds to the current angle)
-var picAddAngle = function(picID=1, angle=0) {
-    $gameScreen.picture(picID)._angle += angle;
-}
+var picAddAngle = function(picID=1, angle=0, origin=0) {
+    var pic = _jakeGetPic(picID);
+    if (pic) {
+        pic._jakeCancelAngleAnim();
+        _jakeApplyOriginIfChanged(pic, origin);
+        pic._angle += angle;
+    }
+};
 
 // ================ Set Angle (sets the angle)
-var picSetAngle = function(picID=1, angle=0) {
-    $gameScreen.picture(picID)._angle = angle;
-}
+var picSetAngle = function(picID=1, angle=0, origin=0) {
+    var pic = _jakeGetPic(picID);
+    if (pic) {
+        pic._jakeCancelAngleAnim();
+        _jakeApplyOriginIfChanged(pic, origin);
+        pic._angle = angle;
+    }
+};
+
+// ================ Add Angle by Speed (adds to the current angle, with visual movement)
+var picAddAngleBySpeed = function(picID=1, angle=0, origin=0, speed=1.5, easeInTime=0, easeOutTime=0) {
+    var pic = _jakeGetPic(picID);
+    if (pic) {
+        _jakeApplyOriginIfChanged(pic, origin);
+        pic._jakeStartAngleAnimBySpeed(pic._angle + angle, speed, easeInTime, easeOutTime);
+    }
+};
+
+// ================ Set Angle by Speed (sets the angle, with visual movement)
+var picSetAngleBySpeed = function(picID=1, angle=0, origin=0, speed=1.5, easeInTime=0, easeOutTime=0) {
+    var pic = _jakeGetPic(picID);
+    if (pic) {
+        _jakeApplyOriginIfChanged(pic, origin);
+        pic._jakeStartAngleAnimBySpeed(angle, speed, easeInTime, easeOutTime);
+    }
+};
+
+// ================ Add Angle by Time (adds to the current angle, with visual movement)
+var picAddAngleByTime = function(picID=1, angle=0, origin=0, time=60) {
+    var pic = _jakeGetPic(picID);
+    if (pic) {
+        _jakeApplyOriginIfChanged(pic, origin);
+        pic._jakeStartAngleAnimByTime(pic._angle + angle, time);
+    }
+};
+
+// ================ Set Angle by Time (sets the angle, with visual movement)
+var picSetAngleByTime = function(picID=1, angle=0, origin=0, time=60) {
+    var pic = _jakeGetPic(picID);
+    if (pic) {
+        _jakeApplyOriginIfChanged(pic, origin);
+        pic._jakeStartAngleAnimByTime(angle, time);
+    }
+};
+
+;(function() {
+
+    var _Game_Picture_initRotation = Game_Picture.prototype.initRotation;
+    Game_Picture.prototype.initRotation = function() {
+        _Game_Picture_initRotation.call(this);
+        this._jakeAngleAnimActive = false;
+    };
+
+    Game_Picture.prototype._jakeCancelAngleAnim = function() {
+        this._jakeAngleAnimActive = false;
+    };
+
+    Game_Picture.prototype._jakeStartAngleAnimBySpeed = function(targetAngle, speed, easeInTime, easeOutTime) {
+        this._rotationSpeed = 0;
+        this._jakeAngleAnimActive = true;
+        this._jakeAngleAnimMode = 'speed';
+        this._jakeAngleAnimTarget = targetAngle;
+        this._jakeAngleAnimSpeed = speed;
+        this._jakeAngleAnimEaseIn = easeInTime;
+        this._jakeAngleAnimEaseOut = easeOutTime;
+        this._jakeAngleAnimElapsed = 0;
+    };
+
+    Game_Picture.prototype._jakeStartAngleAnimByTime = function(targetAngle, time) {
+        this._rotationSpeed = 0;
+        this._jakeAngleAnimActive = true;
+        this._jakeAngleAnimMode = 'time';
+        this._jakeAngleAnimTarget = targetAngle;
+        this._jakeAngleAnimStartAngle = this._angle;
+        this._jakeAngleAnimTime = time;
+        this._jakeAngleAnimElapsed = 0;
+    };
+
+    Game_Picture.prototype._jakeUpdateAngleAnim = function() {
+        if (!this._jakeAngleAnimActive) {
+            return;
+        }
+        if (this._jakeAngleAnimMode === 'time') {
+            this._jakeUpdateAngleAnimByTime();
+        } else {
+            this._jakeUpdateAngleAnimBySpeed();
+        }
+    };
+
+    Game_Picture.prototype._jakeUpdateAngleAnimByTime = function() {
+        var time = this._jakeAngleAnimTime;
+        if (time <= 0) {
+            this._angle = this._jakeAngleAnimTarget;
+            this._jakeAngleAnimActive = false;
+            return;
+        }
+        this._jakeAngleAnimElapsed++;
+        var t = Math.min(this._jakeAngleAnimElapsed / time, 1);
+        this._angle = this._jakeAngleAnimStartAngle +
+            (this._jakeAngleAnimTarget - this._jakeAngleAnimStartAngle) * t;
+        if (t >= 1) {
+            this._jakeAngleAnimActive = false;
+        }
+    };
+
+    Game_Picture.prototype._jakeUpdateAngleAnimBySpeed = function() {
+        var target = this._jakeAngleAnimTarget;
+        var diff = target - this._angle;
+        if (Math.abs(diff) < 0.001) {
+            this._angle = target;
+            this._jakeAngleAnimActive = false;
+            return;
+        }
+        var dist = Math.abs(diff);
+        var sign = diff > 0 ? 1 : -1;
+        var speed = this._jakeAngleAnimSpeed;
+        var easeIn = this._jakeAngleAnimEaseIn;
+        var easeOut = this._jakeAngleAnimEaseOut;
+        var elapsed = this._jakeAngleAnimElapsed;
+        var maxStep = speed;
+        if (easeIn > 0 && elapsed < easeIn) {
+            maxStep = speed * (elapsed + 1) / easeIn;
+        }
+        if (easeOut > 0) {
+            var easeOutDist = speed * easeOut / 2;
+            if (dist <= easeOutDist) {
+                var easeOutStep = speed * dist / easeOutDist;
+                if (easeOutStep < maxStep) {
+                    maxStep = easeOutStep;
+                }
+            }
+        }
+        var step = Math.min(maxStep, dist);
+        this._angle += sign * step;
+        this._jakeAngleAnimElapsed++;
+    };
+
+    var _Game_Picture_update = Game_Picture.prototype.update;
+    Game_Picture.prototype.update = function() {
+        _Game_Picture_update.call(this);
+        this._jakeUpdateAngleAnim();
+    };
+
+})();
 
 
 // ================================ Enemy Sprites
